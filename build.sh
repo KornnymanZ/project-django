85d98f37dc10fdaf33f0aaba9fdaf9d69bc37f9d
KornnymanZ <vorrasukrit@gmail.com>
2026-04-03 11:05:39 +0700

    Automate Remote Superuser provisioning

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python projectdjango/manage.py collectstatic --no-input
 python projectdjango/manage.py migrate
+
+# Automatically spawn a Master Admin account securely bypassing the missing Shell access!
+python projectdjango/manage.py createsuperuser --noinput --username admin --email admin@collaborative.com || true
