77ad18f2682e6a5088aff1f1b6c83b1a99010cd7
KornnymanZ <vorrasukrit@gmail.com>
2026-04-03 14:09:18 +0700

    Fix: Indentation and modern STORAGES API for Cloudinary

--- projectdjango/projectdjango/settings.py
+++ projectdjango/projectdjango/settings.py
@@ -197,7 +197,23 @@ if os.environ.get('CLOUDINARY_CLOUD_NAME'):
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
     }
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+else:
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
 
 # Background Task Queue Configuration (Django-Q2 ORM Broker)
 Q_CLUSTER = {
